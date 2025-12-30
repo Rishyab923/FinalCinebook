317125303bf4ed1d1e61ede6150befb619834294
Rishyab923 <rishyabhuded@gmail.com>
2025-12-30 16:45:18 +0530

    Update axiosWrapper.js

--- bms-frontend/src/apis/axiosWrapper.js
+++ bms-frontend/src/apis/axiosWrapper.js
@@ -1,22 +1,16 @@
 import axios from "axios";
 
-const defaultHeader = {
-  "Content-Type": "application/json",
-  Accept: "application/json",
-};
-
-export const axiosWrapper = axios.create({
+/**
+ * Central Axios instance for the entire app
+ * Uses backend URL from environment variable
+ */
+const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
-  headers: { ...defaultHeader },
-});
-
-
-const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:9000/api",
   headers: {
     "Content-Type": "application/json",
+    Accept: "application/json",
   },
 });
 
-export default axiosInstance;
+export default api;
